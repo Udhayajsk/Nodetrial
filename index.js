850a56e2c8ab50597118575ad5893fea5d9b2002
Udhayajsk <154896176+Udhayajsk@users.noreply.github.com>
2024-04-18 22:12:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 import express from "express";
+import fs from "fs";
+import { format } from "date-fns";
 
 const app = express();
 const PORT = 4000; //http://localhost:4000/
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome you all to my shop" });
+  res.status(200).json({ message: "Welcome you all" });
 });
 
-app.get("/get_data", (req, res) => {
-    res.status(200).json({ message: "data",data:{welcome:"hello"} });
+
+
+app.get("/write", (req, res) => {
+  let today = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
+  const filepath = `Timestamp/${today}.txt`;
+  fs.writeFileSync(filepath, `${today}`, "utf-8");
+  let data = fs.readFileSync(filepath, "utf-8");
+  res.status(200).send(data);
+});
+
+app.get('/read', (req, res) => {
+  let timestamp = [];
+  fs.readdirSync('./Timestamp').forEach(file => {
+      timestamp.push(file);
   });
+  res.status(200).json({ timestamp });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port  ${PORT}`);
 });
